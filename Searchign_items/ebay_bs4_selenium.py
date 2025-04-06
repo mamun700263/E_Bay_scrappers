3900369230ad73254bbc4afc4d7d79109a4a58c1
mamun700263 <mamun700263@gmail.com>
2025-04-06 19:51:49 +0600

    deleted all other venvs and created a single one

---
+++ Searchign_items/ebay_bs4_selenium.py
@@ -0,0 +1,9 @@
+import random 
+import time
+from lxml import html
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
